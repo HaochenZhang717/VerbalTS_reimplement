654f75f97df5803eeb4c0e39e82fab1e0502a1c9
Haochen Zhang <hz112@rice.edu>
2026-02-25 23:47:14 -0600

    0226

---
+++ scripts/synth_u/train.sh
@@ -0,0 +1,16 @@
+CUDA_VISIBLE_DEVICES=5 python run.py \
+    --cond_modal text \
+    --training_stage finetune \
+    --save_folder ./save/synth_u/text2ts_msmdiffmv \
+    --model_diff_config_path configs/synth_u/diff/model_text2ts_dep.yaml \
+    --model_cond_config_path configs/synth_u/cond/text_msmdiffmv.yaml \
+    --train_config_path configs/synth_u/train.yaml \
+    --evaluate_config_path configs/synth_u/evaluate.yaml \
+    --data_folder /playpen/haochenz/synthetic_u \
+    --clip_folder "" \
+    --multipatch_num 3 \
+    --L_patch_len 3 \
+    --base_patch 4 \
+    --epochs 700 \
+    --batch_size 512 \
+    --clip_cache_path ""
