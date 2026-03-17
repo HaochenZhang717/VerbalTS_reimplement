16df17584f727c40a8dd2e1b3a150e12bd747f38
Haochen Zhang <hz112@rice.edu>
2026-03-17 11:41:52 -0500

    0317

--- scripts/qwen/train_cond_synthm.sh
+++ scripts/qwen/train_cond_synthm.sh
@@ -1,6 +1,6 @@
 CUDA_VISIBLE_DEVICES=4 python run_qwen.py \
     --cond_modal text \
-    --training_stage finetune_debug \
+    --training_stage finetune \
     --save_folder ./verbalts_orig_save/synth_m_my_caps/text2ts_msmdiffmv \
     --model_diff_config_path configs/synth_m_debug/diff/model_text2ts_dep.yaml \
     --model_cond_config_path configs/synth_m_debug/cond/text_msmdiffmv.yaml \
@@ -14,4 +14,4 @@ CUDA_VISIBLE_DEVICES=4 python run_qwen.py \
     --epochs 2500 \
     --batch_size 512 \
     --clip_cache_path "" \
-    --samples_name "samples.pt"
+    --samples_name "real_text_samples.pt"
