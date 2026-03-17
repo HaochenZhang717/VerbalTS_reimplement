0282bf67c0120d43228db49c6a78f17ad352c51c
Haochen Zhang <hz112@rice.edu>
2026-03-17 10:41:38 -0500

    0317

--- scripts/synth_u/train_uncond.sh
+++ scripts/synth_u/train_uncond.sh
@@ -21,10 +21,10 @@ CUDA_VISIBLE_DEVICES=1 python run.py \
     --cond_modal text \
     --training_stage pretrain \
     --save_folder ./verbalts_orig_save/uncond_synth_m/text2ts_msmdiffmv \
-    --model_diff_config_path configs/synth_m/diff/model_text2ts_dep.yaml \
-    --model_cond_config_path configs/synth_m/cond/text_msmdiffmv.yaml \
-    --train_config_path configs/synth_m/train.yaml \
-    --evaluate_config_path configs/synth_m/evaluate.yaml \
+    --model_diff_config_path configs/synth_m_debug/diff/model_text2ts_dep.yaml \
+    --model_cond_config_path configs/synth_m_debug/cond/text_msmdiffmv.yaml \
+    --train_config_path configs/synth_m_debug/train.yaml \
+    --evaluate_config_path configs/synth_m_debug/evaluate.yaml \
     --data_folder /playpen/haochenz/LitsDatasets/128_len_ts/synthetic_m \
     --clip_folder "" \
     --multipatch_num 3 \
@@ -39,10 +39,10 @@ CUDA_VISIBLE_DEVICES=1 python run.py \
     --cond_modal text \
     --training_stage pretrain \
     --save_folder ./verbalts_orig_save/uncond_istanbul_traffic/text2ts_msmdiffmv \
-    --model_diff_config_path configs/istanbul_traffic/diff/model_text2ts_dep.yaml \
-    --model_cond_config_path configs/istanbul_traffic/cond/text_msmdiffmv.yaml \
-    --train_config_path configs/istanbul_traffic/train.yaml \
-    --evaluate_config_path configs/istanbul_traffic/evaluate.yaml \
+    --model_diff_config_path configs/istanbul_debug/diff/model_text2ts_dep.yaml \
+    --model_cond_config_path configs/istanbul_debug/cond/text_msmdiffmv.yaml \
+    --train_config_path configs/istanbul_debug/train.yaml \
+    --evaluate_config_path configs/istanbul_debug/evaluate.yaml \
     --data_folder /playpen/haochenz/LitsDatasets/128_len_ts/istanbul_traffic \
     --clip_folder "" \
     --multipatch_num 3 \
