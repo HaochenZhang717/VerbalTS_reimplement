32b85427d5911fcfe61b40071ef5b156ac7de456
Haochen Zhang <hz112@rice.edu>
2026-03-18 10:46:10 -0500

    0318

--- scripts/evaluate/fid_synthu.sh
+++ scripts/evaluate/fid_synthu.sh
@@ -21,6 +21,21 @@ python calculate_fid.py \
 
 
 
+
+REAL_PATH="/playpen/haochenz/VerbalTS_reimplement/verbalts_orig_save/synth_u_qwen/text2ts_msmdiffmv/0/real_text_samples.pt"
+FAKE_PATH="/playpen/haochenz/VerbalTS_reimplement/verbalts_orig_save/synth_u_qwen/text2ts_msmdiffmv/0/real_text_samples.pt"
+python calculate_fid.py \
+    --real_path ${REAL_PATH} \
+    --fake_path ${FAKE_PATH} \
+    --ckpt_path "./fid_vae_ckpts/vae_synth_u/best.pt" \
+    --batch_size 128 \
+    --hidden_size 128 \
+    --num_layers 2 \
+    --num_heads 8 \
+    --latent_dim 64 \
+    --save_path "./fid_results/uncond_generation.txt"
+
+
 REAL_PATH="/playpen/haochenz/VerbalTS_reimplement/verbalts_orig_save/synth_u/text2ts_msmdiffmv/0/verbalts_caps_samples.pt"
 FAKE_PATH="/playpen/haochenz/VerbalTS_reimplement/verbalts_orig_save/synth_u/text2ts_msmdiffmv/0/verbalts_caps_samples.pt"
 python calculate_fid.py \
