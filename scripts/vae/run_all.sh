8aa59f6b9b710d02bf34fe0005321a7b0cf48094
Haochen Zhang <hz112@rice.edu>
2026-03-17 21:46:16 -0500

    0317

--- scripts/vae/run_all.sh
+++ scripts/vae/run_all.sh
@@ -1,2 +1,5 @@
-bash scripts/vae/synth_m.sh
-bash scripts/vae/synth_u.sh
+#bash scripts/vae/synth_m.sh
+#bash scripts/vae/synth_u.sh
+
+bash scripts/vae/extract_embeds_synthu.sh
+bash scripts/vae/extract_embeds_synthm.sh
