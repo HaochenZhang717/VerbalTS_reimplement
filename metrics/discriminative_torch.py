ce8722acd2cd51e8e86854f0649dea2f2503a0f4
Haochen Zhang <hz112@rice.edu>
2026-03-19 18:24:50 -0500

    0319

--- metrics/discriminative_torch.py
+++ metrics/discriminative_torch.py
@@ -153,13 +153,10 @@ def discriminative_score_metrics(ori_data, generated_data, input_size, device,):
         if itt % 100 == 0:
             model.eval()
             with torch.no_grad():
-                breakpoint()
-                print(f"test_x.shape: {test_x.shape}")
-                print(f"test_x_hat.shape: {test_x_hat.shape}")
-                test_x = torch.stack(test_x).to(device)
-                test_x_hat = torch.stack(test_x_hat).to(device)
-                _, y_pred_real_curr = model(test_x.float())
-                _, y_pred_fake_curr = model(test_x_hat.float())
+                test_x_tensor = torch.stack(test_x).to(device).float()
+                test_x_hat_tensor = torch.stack(test_x_hat).to(device).float()
+                _, y_pred_real_curr = model(test_x_tensor)
+                _, y_pred_fake_curr = model(test_x_hat_tensor)
 
                 y_pred_real_curr = y_pred_real_curr.detach().cpu().numpy()
                 y_pred_fake_curr = y_pred_fake_curr.detach().cpu().numpy()
