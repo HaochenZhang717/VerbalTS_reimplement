e487198d99a65c5c8837758b7b600178e403707b
Haochen Zhang <hz112@rice.edu>
2026-03-20 18:36:25 -0500

    0319

--- models/diffusion/verbalts_qwen_v4.py
+++ models/diffusion/verbalts_qwen_v4.py
@@ -390,10 +390,8 @@ class VerbalTSQwenV4(nn.Module):
             print(f"attr_emb_raw.shape = {attr_emb_raw.shape}")
             # attr_emb_list.append(attr_emb_raw.repeat_interleave(32//patch_length, dim=-1))
             # attr_emb_patch = self.attr_embed_upsample[i](attr_emb_raw.permute(0, 2, 3, 1)).reshape(B_raw, n_var, -1, self.channels)
-            attr_emb_patch = self.attr_embed_upsample[i](attr_emb_raw.permute(0, 2, 3, 1)) # torch.Size([512, 1, 4, 8])
-            print(f"attr_emb_patch.shape = {attr_emb_patch.shape}")
-            breakpoint()
-            attr_emb_patch = attr_emb_patch.permute(0,3,1,2) # [512, 1, 4n, 64] -> [512, 64, 1, 4n]
+            attr_emb_patch = self.attr_embed_upsample[i](attr_emb_raw.permute(0, 2, 3, 1)).reshape(B_raw, n_var, -1, self.channels) # torch.Size([512, 1, 4, 512])
+            attr_emb_patch = attr_emb_patch.permute(0,3,1,2) # [512, 1, 4*8, 64] -> [512, 64, 1, 4*8]
             attr_emb_list.append(attr_emb_patch)
             print(f"attr_emb_patch.shape = {attr_emb_patch.shape}")
             breakpoint() # [512, 64, 1, 4] -> [512, 1, 4, 64] -> [512, 1, 4, 64n] -> [512, 1, 4n, 64]
