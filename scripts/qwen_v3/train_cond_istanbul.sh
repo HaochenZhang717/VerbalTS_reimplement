42bd30865d4f5ec3c16c4690acc08ec3d9052e5b
Haochen Zhang <hz112@rice.edu>
2026-03-20 11:57:26 -0500

    0319

--- scripts/qwen_v3/train_cond_istanbul.sh
+++ scripts/qwen_v3/train_cond_istanbul.sh
@@ -3,11 +3,11 @@ export USE_CAUSAL=false
 CUDA_VISIBLE_DEVICES=7 python run_qwen_v3.py \
     --cond_modal text \
     --training_stage finetune \
-    --save_folder ./verbalts_orig_save/istanbul_v3/text2ts_msmdiffmv \
-    --model_diff_config_path configs/istanbul_v3/diff/model_text2ts_dep.yaml \
-    --model_cond_config_path configs/istanbul_v3/cond/text_msmdiffmv.yaml \
-    --train_config_path configs/istanbul_v3/train.yaml \
-    --evaluate_config_path configs/istanbul_v3/evaluate.yaml \
+    --save_folder ./verbalts_orig_save/istanbul_qwen_v3/text2ts_msmdiffmv \
+    --model_diff_config_path configs/istanbul_qwen_v3/diff/model_text2ts_dep.yaml \
+    --model_cond_config_path configs/istanbul_qwen_v3/cond/text_msmdiffmv.yaml \
+    --train_config_path configs/istanbul_qwen_v3/train.yaml \
+    --evaluate_config_path configs/istanbul_qwen_v3/evaluate.yaml \
     --data_folder /playpen/haochenz/LitsDatasets/128_len_ts/istanbul_traffic \
     --clip_folder "" \
     --multipatch_num 3 \
