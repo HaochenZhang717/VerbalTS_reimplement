ed9833909107c20aa1f05b57ab901fdfcc8f6fcc
Haochen Zhang <hz112@rice.edu>
2026-03-20 18:34:43 -0500

    0319

--- models/diffusion/verbalts_qwen_v4.py
+++ models/diffusion/verbalts_qwen_v4.py
@@ -341,8 +341,8 @@ class VerbalTSQwenV4(nn.Module):
             self.attr_embed_upsample.append(
                 SwiGLUFFN(
                     in_features=self.channels,
-                    hidden_features=self.channels * patch_length // 32,
-                    out_features=self.channels * patch_length // 32,
+                    hidden_features=self.channels * 32 // patch_length,
+                    out_features=self.channels * 32 // patch_length,
                     bias=True,
                 )
             )
@@ -390,7 +390,7 @@ class VerbalTSQwenV4(nn.Module):
             print(f"attr_emb_raw.shape = {attr_emb_raw.shape}")
             # attr_emb_list.append(attr_emb_raw.repeat_interleave(32//patch_length, dim=-1))
             # attr_emb_patch = self.attr_embed_upsample[i](attr_emb_raw.permute(0, 2, 3, 1)).reshape(B_raw, n_var, -1, self.channels)
-            attr_emb_patch = self.attr_embed_upsample[i](attr_emb_raw.permute(0, 2, 3, 1))
+            attr_emb_patch = self.attr_embed_upsample[i](attr_emb_raw.permute(0, 2, 3, 1)) # torch.Size([512, 1, 4, 8])
             print(f"attr_emb_patch.shape = {attr_emb_patch.shape}")
             breakpoint()
             attr_emb_patch = attr_emb_patch.permute(0,3,1,2) # [512, 1, 4n, 64] -> [512, 64, 1, 4n]
