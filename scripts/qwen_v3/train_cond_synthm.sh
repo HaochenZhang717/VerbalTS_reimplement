fc7f526f1a80366cc32763979ffc488e441d8869
Haochen Zhang <hz112@rice.edu>
2026-03-20 11:51:53 -0500

    0319

--- scripts/qwen_v3/train_cond_synthm.sh
+++ scripts/qwen_v3/train_cond_synthm.sh
@@ -7,7 +7,7 @@ CUDA_VISIBLE_DEVICES=6 python run_qwen_v3.py \
     --model_diff_config_path configs/synth_m_qwen_v3/diff/model_text2ts_dep.yaml \
     --model_cond_config_path configs/synth_m_qwen_v3/cond/text_msmdiffmv.yaml \
     --train_config_path configs/synth_m_qwen_v3/train.yaml \
-    --evaluate_config_path configs/synth_m_qwen_v3/evaluate.yaml \
+    --evaluate_config_path configs/synth_m_qwen_v3/evaluate_real_text.yaml \
     --data_folder /playpen/haochenz/LitsDatasets/128_len_ts/synthetic_m \
     --clip_folder "" \
     --multipatch_num 3 \
