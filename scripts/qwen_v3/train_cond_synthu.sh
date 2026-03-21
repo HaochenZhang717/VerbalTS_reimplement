9c0699b4fb5318d546c6e9309fe7bcc044a3b6c4
Haochen Zhang <hz112@rice.edu>
2026-03-21 02:08:49 -0500

    0320

--- scripts/qwen_v3/train_cond_synthu.sh
+++ scripts/qwen_v3/train_cond_synthu.sh
@@ -1,21 +1,36 @@
+#!/bin/bash
+
 export USE_CAUSAL=false
 export WANDB_NAME="qwen_v3_synth_u"
 
-CUDA_VISIBLE_DEVICES=7 python run_qwen_v3.py \
-    --cond_modal text \
-    --training_stage finetune \
-    --save_folder ./verbalts_orig_save/synth_u_qwen_v3/text2ts_msmdiffmv \
-    --model_diff_config_path configs/synth_u_qwen_v3/diff/model_text2ts_dep.yaml \
-    --model_cond_config_path configs/synth_u_qwen_v3/cond/text_msmdiffmv.yaml \
-    --train_config_path configs/synth_u_qwen_v3/train.yaml \
-    --evaluate_config_path configs/synth_u_qwen_v3/evaluate.yaml \
-    --data_folder /playpen/haochenz/LitsDatasets/128_len_ts/synthetic_u \
-    --clip_folder "" \
-    --multipatch_num 3 \
-    --L_patch_len 2 \
-    --base_patch 4 \
-    --epochs 2500 \
-    --batch_size 512 \
-    --clip_cache_path "" \
-    --samples_name "real_text_samples.pt" \
-    --model_ckpt_name "model_best_loss.pth"
+LR_LIST=(1e-4 5e-4 1e-3)
+BS_LIST=(128 256 512)
+
+GPU=7
+
+for LR in "${LR_LIST[@]}"
+do
+  for BS in "${BS_LIST[@]}"
+  do
+    echo "Running lr=$LR bs=$BS"
+
+    CUDA_VISIBLE_DEVICES=$GPU python run_qwen_v3.py \
+        --cond_modal text \
+        --training_stage finetune \
+        --save_folder ./sweep/synth_u_qwen_v3/lr_${LR}_bs_${BS} \
+        --model_diff_config_path configs/synth_u_qwen_v3/diff/model_text2ts_dep.yaml \
+        --model_cond_config_path configs/synth_u_qwen_v3/cond/text_msmdiffmv.yaml \
+        --train_config_path configs/synth_u_qwen_v3/train.yaml \
+        --evaluate_config_path configs/synth_u_qwen_v3/evaluate.yaml \
+        --data_folder /playpen/haochenz/LitsDatasets/128_len_ts/synthetic_u \
+        --multipatch_num 3 \
+        --L_patch_len 2 \
+        --base_patch 4 \
+        --epochs 2500 \
+        --batch_size $BS \
+        --lr $LR \
+        --samples_name "real_text_samples.pt" \
+        --model_ckpt_name "model_best_loss.pth" \
+        --n_runs 1
+  done
+done
