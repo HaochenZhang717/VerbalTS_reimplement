c8d3a9f281fcec08b7fca9fd91f8614fe26b404e
Haochen Zhang <hz112@rice.edu>
2026-03-21 12:52:22 -0500

    0321

--- plot_all/plot_fid_old.py
+++ plot_all/plot_fid_old.py
@@ -1,10 +1,10 @@
 import re
 import matplotlib.pyplot as plt
 
-def parse_fid_file(path, score_name="FID"):
+def parse_fid_file(path):
     with open(path, "r") as f:
         text = f.read()
-    breakpoint()
+
     blocks = text.split("--------------------------")
 
     data = []
@@ -14,23 +14,24 @@ def parse_fid_file(path, score_name="FID"):
         if len(lines) == 0:
             continue
 
-        # 找 epoch
+        # 第一行：文件名
         name = lines[0]
-        match_epoch = re.search(r"epoch_(\d+)", name)
+
+        # 修复：匹配 Epoch1099
+        match_epoch = re.search(r"[Ee]poch[_]?(\d+)", name)
 
         if "best_loss" in name:
-            epoch = -1  # 用 -1 表示 best
+            epoch = -1
         elif match_epoch:
             epoch = int(match_epoch.group(1))
         else:
             continue
 
-
         # 找 FID
         fid = None
         for line in lines:
-            if score_name in line:
-                match_fid = re.search(r"\$([0-9.]+)", line)
+            if "FID" in line:
+                match_fid = re.search(r"FID:\s*\$([0-9.]+)", line)
                 if match_fid:
                     fid = float(match_fid.group(1))
                     break
@@ -38,11 +39,8 @@ def parse_fid_file(path, score_name="FID"):
         if fid is not None:
             data.append((epoch, fid))
 
-    # 排序（best放最前）
     data.sort(key=lambda x: x[0])
-
     return data
-
 
 def plot_fid(data, save_name):
     epochs = [x[0] for x in data]
@@ -60,24 +58,11 @@ def plot_fid(data, save_name):
 
 
 if __name__ == "__main__":
-    # path = "../fid_results/synth_u_imagen_time.txt"  # 改成你的路径
-    # data_fid = parse_fid_file(path, score_name="FID")
-    # data_kid = parse_fid_file(path, score_name="KID")
-    #
-    # # print("Parsed data:")
-    # # for d in data:
-    # #     print(d)
-    #
-    # plot_fid(data_fid, save_name="synth_u_imagen_time_fid.png")
-    # plot_fid(data_kid, save_name="synth_u_imagen_time_kid.png")
-
     path = "../fid_results/synth_u_qwen_v1_generation_run1.txt"  # 改成你的路径
-    data_fid = parse_fid_file(path, score_name="FID")
-    data_kid = parse_fid_file(path, score_name="KID")
+    data = parse_fid_file(path)
 
     print("Parsed data:")
-    for d in data_fid:
+    for d in data:
         print(d)
 
-    plot_fid(data_fid, save_name="synth_u_qwen_v1_generation_run1_fid.png")
-    plot_fid(data_kid, save_name="synth_u_qwen_v1_generation_run1_kid.png")
+    plot_fid(data, save_name="synth_u_qwen_v1_generation_run1.png")
