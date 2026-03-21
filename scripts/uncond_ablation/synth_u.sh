e6dd99e8ac180db5e504445b364731795d378526
Haochen Zhang <hz112@rice.edu>
2026-03-21 09:50:13 -0500

    0320

--- scripts/uncond_ablation/synth_u.sh
+++ scripts/uncond_ablation/synth_u.sh
@@ -1,4 +1,15 @@
-CUDA_VISIBLE_DEVICES=4 python run.py \
+LR_LIST=(5e-4 2e-4)
+BS_LIST=(256 512)
+
+for LR in "${LR_LIST[@]}"
+do
+  for BS in "${BS_LIST[@]}"
+  do
+    echo "Running lr=$LR bs=$BS"
+
+    export WANDB_NAME="uncond_synth_u_cosine-lr${LR}_bs${BS}"
+
+    CUDA_VISIBLE_DEVICES=4 python run.py \
     --cond_modal text \
     --training_stage pretrain \
     --save_folder ./sweep/uncond_synth_u/text2ts_msmdiffmv \
@@ -12,47 +23,9 @@ CUDA_VISIBLE_DEVICES=4 python run.py \
     --L_patch_len 2 \
     --base_patch 4 \
     --epochs 2500 \
-    --batch_size 512 \
+    --batch_size ${BS} \
+    --lr ${LR} \
     --clip_cache_path "" \
-    --only_evaluate True \
     --samples_name "samples.pt"
-
-#CUDA_VISIBLE_DEVICES=3 python run_debug.py \
-#    --cond_modal text \
-#    --training_stage pretrain \
-#    --save_folder ./verbalts_orig_save/uncond_synth_m/text2ts_msmdiffmv \
-#    --model_diff_config_path configs/synth_m_debug/diff/model_text2ts_dep.yaml \
-#    --model_cond_config_path configs/synth_m_debug/cond/text_msmdiffmv.yaml \
-#    --train_config_path configs/synth_m_debug/train.yaml \
-#    --evaluate_config_path configs/synth_m_debug/evaluate.yaml \
-#    --data_folder /playpen/haochenz/LitsDatasets/128_len_ts/synthetic_m \
-#    --clip_folder "" \
-#    --multipatch_num 3 \
-#    --L_patch_len 2 \
-#    --base_patch 4 \
-#    --epochs 2500 \
-#    --batch_size 512 \
-#    --clip_cache_path "" \
-#    --samples_name "samples.pt" \
-#    --only_evaluate True \
-#    --n_runs 1
-#
-#  CUDA_VISIBLE_DEVICES=3 python run_debug.py \
-#    --cond_modal text \
-#    --training_stage pretrain \
-#    --save_folder ./verbalts_orig_save/uncond_istanbul_traffic/text2ts_msmdiffmv \
-#    --model_diff_config_path configs/istanbul_debug/diff/model_text2ts_dep.yaml \
-#    --model_cond_config_path configs/istanbul_debug/cond/text_msmdiffmv.yaml \
-#    --train_config_path configs/istanbul_debug/train.yaml \
-#    --evaluate_config_path configs/istanbul_debug/evaluate.yaml \
-#    --data_folder /playpen/haochenz/LitsDatasets/128_len_ts/istanbul_traffic \
-#    --clip_folder "" \
-#    --multipatch_num 3 \
-#    --L_patch_len 2 \
-#    --base_patch 4 \
-#    --epochs 2500 \
-#    --batch_size 512 \
-#    --clip_cache_path "" \
-#    --samples_name "samples.pt" \
-#    --only_evaluate True \
-#    --n_runs 1
+done
+done
